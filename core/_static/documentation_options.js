5f4b05ff08ee3bb95dbe5b7e53f96551ad2d373c
rossberg <rossberg@users.noreply.github.com>
2021-09-14 11:53:06 +0000

    deploy: 7b1ee9bc94ebf9783d08f0d5022d1963ab0442b8

---
+++ core/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '1.1 + function-references + gc (Draft 2021-09-14)',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: false,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
