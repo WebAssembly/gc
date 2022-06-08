d7526bfc47f15fec5935cbd008a051ae75555250
Andreas Rossberg <rossberg@mpi-sws.org>
2022-06-08 15:44:38 +0200

    Defer explicit RTTs

--- interpreter/runtime/rtt.mli
+++ interpreter/runtime/rtt.mli
@@ -11,6 +11,7 @@ val alloc : sem_var -> rtt
 val type_inst_of : rtt -> sem_var
 val ctx_type_of : rtt -> ctx_type
 
+val eq_rtt : rtt -> rtt -> bool
 val match_rtt : rtt -> rtt -> bool
 
 val string_of_rtt : rtt -> string
