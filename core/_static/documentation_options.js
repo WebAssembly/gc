9c1b80e4d4a594136b6b40b1a4d0f9a9791bb67a
tlively <tlively@users.noreply.github.com>
2023-08-09 15:56:31 +0000

    deploy: 1d9585f29845a1bfbc7d6f17b51aeb272b6e28fd

--- core/_static/documentation_options.js
+++ core/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.0 + tail calls + function references + gc (Draft 2023-07-31)',
+    VERSION: '2.0 + tail calls + function references + gc (Draft 2023-08-09)',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
